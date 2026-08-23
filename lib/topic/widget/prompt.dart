f12f9d43b3020a5fbb843364ac9d75feac7a88b7
clragon <clragon@clynamic.net>
2026-08-23 14:36:21 +0200

    simplified topic dialog and sheet

--- lib/topic/widget/prompt.dart
+++ lib/topic/widget/prompt.dart
@@ -4,77 +4,45 @@ import 'package:e1547/tag/tag.dart';
 import 'package:e1547/topic/topic.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:sliding_sheet/sliding_sheet.dart';
 
 Future<void> showTopicPrompt({
   required BuildContext context,
   required Topic topic,
-}) async {
-  if (Theme.of(context).isDesktop) {
-    showTopicDialog(context: context, topic: topic);
-  } else {
-    showTopicSheet(context: context, topic: topic);
-  }
-}
-
-void showTopicSheet({required BuildContext context, required Topic topic}) {
-  showSlidingBottomSheet(
-    context,
-    builder: (context) => defaultSlidingSheetDialog(
-      context,
-      (context, sheetState) => TopicSheet(topic: topic),
-    ),
-  );
-}
-
-class TopicSheet extends StatelessWidget {
-  const TopicSheet({super.key, required this.topic});
-
-  final Topic topic;
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Text(
-                      tagToRaw(topic.title),
-                      style: Theme.of(context).textTheme.titleLarge,
-                      softWrap: true,
-                    ),
-                  ),
-                ),
-                ActionButton(
-                  icon: const Icon(Icons.share),
-                  onTap: () async => Share.text(
-                    context,
-                    context.read<Client>().withHost(topic.link),
-                  ),
-                  label: const Text('Share'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            const Divider(),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: TopicInfo(topic: topic),
-            ),
-          ],
+}) => showPrompt<void>(
+  context,
+  dialogWidth: 800,
+  header: (context) => Padding(
+    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+    child: OverflowBar(
+      alignment: MainAxisAlignment.spaceBetween,
+      overflowSpacing: 8,
+      children: [
+        Text(
+          tagToRaw(topic.title),
+          style: Theme.of(context).textTheme.titleLarge,
+          softWrap: true,
         ),
+        ActionButton(
+          icon: const Icon(Icons.share),
+          onTap: () async =>
+              Share.text(context, context.read<Client>().withHost(topic.link)),
+          label: const Text('Share'),
+        ),
+      ],
+    ),
+  ),
+  body: Column(
+    mainAxisSize: MainAxisSize.min,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      const Divider(),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: TopicInfo(topic: topic),
       ),
-    );
-  }
-}
+    ],
+  ),
+);
 
 class TopicInfo extends StatelessWidget {
   const TopicInfo({super.key, required this.topic});
@@ -129,63 +97,6 @@ class TopicInfo extends StatelessWidget {
             DateFormatting.dateTime(topic.updatedAt.toLocal()),
           ),
         ],
-      ),
-    );
-  }
-}
-
-Future<void> showTopicDialog({
-  required BuildContext context,
-  required Topic topic,
-}) async {
-  await showDialog(
-    context: context,
-    builder: (context) => TopicDialog(topic: topic),
-  );
-}
-
-class TopicDialog extends StatelessWidget {
-  const TopicDialog({super.key, required this.topic});
-
-  final Topic topic;
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      content: SizedBox(
-        width: 800,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Text(
-                      tagToRaw(topic.title),
-                      style: Theme.of(context).textTheme.titleLarge,
-                      softWrap: true,
-                    ),
-                  ),
-                ),
-                ActionButton(
-                  icon: const Icon(Icons.share),
-                  onTap: () async => Share.text(
-                    context,
-                    context.read<Client>().withHost(topic.link),
-                  ),
-                  label: const Text('Share'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            const Divider(),
-            TopicInfo(topic: topic),
-          ],
-        ),
       ),
     );
   }
